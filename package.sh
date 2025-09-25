60c58ba58caa89c78161e35d6f34914831987a6f
CrazySpottedDove <3230104178@zju.edu.cn>
2025-09-25 17:01:31 +0800

    添加直接可用的lua脚本，用于合并并排序数据表，并对string做了对应处理

--- package.sh
+++ package.sh
@@ -45,7 +45,7 @@ zip "$OUTPUT_ZIP" "./存档位置.lnk"
 zip "$OUTPUT_ZIP" "./游玩必读说明，务必阅读.url"
 
 # tmp usage
-zip "$OUTPUT_ZIP" "./_assets/kr1-desktop/images/fullhd/go_towers_dark_elf-1.dds"
+# zip "$OUTPUT_ZIP" "./_assets/kr1-desktop/images/fullhd/go_towers_dark_elf-1.dds"
 # zip "$OUTPUT_ZIP" "./_assets/kr1-desktop/images/fullhd/go_towers_tricannon-1.dds"
 # zip "$OUTPUT_ZIP" "./_assets/kr1-desktop/images/fullhd/gui_common-5.png"
 # # 记录 commit hash 文件
