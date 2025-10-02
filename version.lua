099f4a9b684f9e07c044bfcc98acc6683b93bc89
CrazySpottedDove <3230104178@zju.edu.cn>
2025-10-02 17:18:27 +0800

    VERSION JUMP

--- version.lua
+++ version.lua
@@ -15,4 +15,4 @@ version.bundle_id = "com.ironhidegames.kingdomrush.standalone"
 version.vc = "kr1-desktop-5.6.12"
 version.build = v
 version.bundle_keywords = "-standalone"
-version.id = "5.0.0"
+version.id = "5.0.1"
