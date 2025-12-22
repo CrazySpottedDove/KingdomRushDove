b458f88143e9096312b4cfe1752ada22e0bffd10
MC123ACD <1729417534@qq.com>
2025-12-22 13:54:35 +0800

    修复电涌一技能相同问题

--- kr1/tower_scripts.lua
+++ kr1/tower_scripts.lua
@@ -18046,7 +18046,7 @@ function scripts.tower_sparking_geode.update(this, store, script)
 		end
 
 		if U.find_first_enemy_in_range_filter_off(this.pos, a.range, a_crystalize.vis_flags, a_crystalize.vis_bans) == nil then
-			a_crystalize.ts = store.tick_ts + fts(10)
+			a_crystalize.ts = a_crystalize.ts + fts(10)
 
 			return false
 		end
