96fa7c890d08c6db8ef921267c7a77814a114c56
CrazySpottedDove <3230104178@zju.edu.cn>
2026-06-28 08:51:41 +0800

    feat: version->2.0.3.7

--- version.lua
+++ version.lua
@@ -8,7 +8,7 @@ local version = {
 	bundle_id = "com.ironhidegames.kingdomrush.standalone",
 	build = is_build,
 	bundle_keywords = "-standalone",
-	id = "2.0.3.6"
+	id = "2.0.3.7"
 }
 
 return version
