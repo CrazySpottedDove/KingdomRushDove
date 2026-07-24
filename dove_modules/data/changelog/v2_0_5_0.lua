eaa0336d8475bfab922730d502eccf6e153c06ff
CrazySpottedDove <3230104178@zju.edu.cn>
2026-07-24 19:10:38 +0800

    perf: 删除更多冗余的四代防御塔音效

--- dove_modules/data/changelog/v2_0_5_0.lua
+++ dove_modules/data/changelog/v2_0_5_0.lua
@@ -1,1 +1,1 @@
-return {{date="2026-07-24",author="CrazySpottedDove",message="fix: 修正维鲁克的点击框范围"},{date="2026-07-24",author="CrazySpottedDove",message="perf: 删除冗余的四代防御塔音效"}}
+return {{date="2026-07-24",author="CrazySpottedDove",message="fix: 修正维鲁克的点击框范围"},{date="2026-07-24",author="CrazySpottedDove",message="perf: 删除冗余的四代防御塔音效"},{date="2026-07-24",author="CrazySpottedDove",message="perf: 删除更多冗余的四代防御塔音效"}}
