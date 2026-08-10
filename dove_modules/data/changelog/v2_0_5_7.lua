916af8fef4fa984e551163507e1bebfdfc862d14
CrazySpottedDove <3230104178@zju.edu.cn>
2026-08-10 16:46:27 +0800

    balance: 提高电云10%移速

--- dove_modules/data/changelog/v2_0_5_7.lua
+++ dove_modules/data/changelog/v2_0_5_7.lua
@@ -1,1 +1,1 @@
-return {{date="2026-08-10",author="CrazySpottedDove",message="feat: 新增英雄：墨忒弥斯"}}
+return {{date="2026-08-10",author="CrazySpottedDove",message="feat: 新增英雄：墨忒弥斯"},{date="2026-08-10",author="CrazySpottedDove",message="balance: 提高电云10%移速"}}
