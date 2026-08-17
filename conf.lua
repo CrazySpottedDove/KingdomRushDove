51b50ffa514febdbec33961a1d565ec81e8fbec4
CrazySpottedDove <3230104178@zju.edu.cn>
2026-08-17 09:14:23 +0800

    chore: 取消追踪args.lua

--- conf.lua
+++ conf.lua
@@ -1,16 +1,8 @@
 -- chunkname: @./conf.lua
-local enabled_console
-
-if arg[2] == "debug" or arg[2] == "release" then
-	enabled_console = false
-else
-	enabled_console = true
-end
-
 function love.conf(t)
 	t.modules.physics = false
 	t.modules.joystick = false
 	t.modules.video = false
 	t.accelerometerjoystick = false
-	t.console = enabled_console
+	t.console = true
 end
---
+++ dove_modules/data/changelog/v2_0_6_1.lua
@@ -0,0 +1,1 @@
+return {{date="2026-08-17",author="CrazySpottedDove",message="chore: 取消追踪args.lua"}}
