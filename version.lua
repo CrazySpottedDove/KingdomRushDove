ef31acf66e0dc16652a0c44edb177919552caec4
CrazySpottedDove <3230104178@zju.edu.cn>
2026-09-09 23:15:18 +0800

    feat: version->2.0.7.3(建议重新下载本体！)

--- version.lua
+++ version.lua
@@ -8,7 +8,7 @@ local version = {
 	bundle_id = "com.ironhidegames.kingdomrush.standalone",
 	build = is_build,
 	bundle_keywords = "-standalone",
-	id = "2.0.7.2"
+	id = "2.0.7.3"
 }
 
 return version
