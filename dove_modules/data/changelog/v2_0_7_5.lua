37abf44e87562e961a62253959c338c2d4da1db4
CrazySpottedDove <3230104178@zju.edu.cn>
2026-09-15 17:38:07 +0800

    fix: 修正弩堡buff作用范围的中心

---
+++ dove_modules/data/changelog/v2_0_7_5.lua
@@ -0,0 +1,1 @@
+return {{date="2026-09-15",author="CrazySpottedDove",message="fix: 修正弩堡buff作用范围的中心"}}
--- kr1/tower_scripts.lua
+++ kr1/tower_scripts.lua
@@ -694,7 +694,7 @@ scripts.tower_crossbow = {
 
 					local range = ea.range + km.clamp(1, 3, pow_e.level + 1) * ea.range_inc
 					local targets = table.filter(store.towers, function(_, e)
-						return not table.arraycontains(modded_ids, e.id) and U.is_inside_ellipse(e.pos, tpos, range)
+						return not table.arraycontains(modded_ids, e.id) and U.is_inside_ellipse(e.pos, this.pos, range)
 					end)
 
 					for _, target in ipairs(targets) do
@@ -743,7 +743,7 @@ scripts.tower_crossbow = {
 							return v.modifier.target_id
 						end)
 						local towers = table.filter(store.towers, function(_, e)
-							return e.tower.can_be_mod and not table.contains(busy_ids, e.id) and U.is_inside_ellipse(e.pos, tpos, eagle_range)
+							return e.tower.can_be_mod and not table.contains(busy_ids, e.id) and U.is_inside_ellipse(e.pos, this.pos, eagle_range)
 						end)
 
 						for _, tower in ipairs(towers) do
