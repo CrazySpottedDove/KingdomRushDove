992040530c9932f9dc83ad3e0fe2ac70f937d051
CrazySpottedDove <3230104178@zju.edu.cn>
2026-09-25 17:28:05 +0800

    feat: 添加毒伤icon

--- dove_modules/data/changelog/v2_0_7_8.lua
+++ dove_modules/data/changelog/v2_0_7_8.lua
@@ -1,1 +1,1 @@
-return {{date="2026-09-25",author="CrazySpottedDove",message="fix: 修复部分纹理偏移问题"}}
+return {{date="2026-09-25",author="CrazySpottedDove",message="fix: 修复部分纹理偏移问题"},{date="2026-09-25",author="CrazySpottedDove",message="feat: 添加毒伤icon"}}
